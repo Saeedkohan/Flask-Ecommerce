e00acc794f78d09e8cdf0e44c5edabd9e2562aab
Saeed Kohansal <saeedkohan8520@gmail.com>
2025-05-22 22:00:45 +0330

    Creating Templates

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,12 @@ def create_app() :
     app=Flask(__name__)
     app.config["SECRET_KEY"]="KDK WED JKEJFKEJF"
 
+    from .views import views
+    from .admin import admin
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(admin, url_prefix='/')
+
     return  app
