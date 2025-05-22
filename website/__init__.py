9302911da601ddba25522cb685c569a27dde6bff
Saeed Kohansal <saeedkohan8520@gmail.com>
2025-05-23 01:16:34 +0330

    Creating Models / Database

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db=SQLAlchemy()
+DB_NAME='database.sqlite3'
+
+def create_database():
+    db.create_all()
+    print('Database created')
 
 
 def create_app() :
     app=Flask(__name__)
     app.config["SECRET_KEY"]="KDK WED JKEJFKEJF"
+    app.config["SQLALCHEMY_DATABASE_URI"]=f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .admin import admin
@@ -12,5 +23,9 @@ def create_app() :
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(admin, url_prefix='/')
+
+
+    with app.app_context():
+        create_database()
 
     return  app
